6e3bae913bda276d78a5967071c1c084b699eb72
kritiaiwebsite <founder@kritiai.in>
2024-08-13 22:18:07 +0530

    second

--- server/app.js
+++ server/app.js
@@ -36,7 +36,7 @@ app.use("/uploads", express.static(__dirname+"/uploads")); // For getting images
 
 app.use(cors({
     credentials:true,
-    origin:'http://localhost:3000'||'https://airbnb-1-bpw0.onrender.com'
+    origin:'https://airbnb-1-bpw0.onrender.com'
 }));
 
 
